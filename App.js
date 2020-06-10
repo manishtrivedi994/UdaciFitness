0df61b0e693c72ff2c763b5427a1bea376f3f359
Manish Trivedi <manishtrivedi994@gmail.com>
2020-06-10 16:04:28 +0530

    Udaci Fitness AddEntry Method

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { View } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+import AddEntry from './components/AddEntry'
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -8,29 +10,9 @@ const instructions = Platform.select({
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
+    <View>
+      <AddEntry/>
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});

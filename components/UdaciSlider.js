c5838a81171399d3e461e612edb4c22077338a22
Manish Trivedi <manishtrivedi994@gmail.com>
2020-06-10 16:50:20 +0530

    Changes for AddEntry Render

---
+++ components/UdaciSlider.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function UdaciSilder() {
+    return (
+        <View>
+            <Text>UdaciSilder</Text>
+        </View>
+    )
+}

aabe3a9e62d8e21fccc50c70215178d5416ceb4c
Manish Trivedi <manishtrivedi994@gmail.com>
2020-06-12 22:52:13 +0530

    Changes for History

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -15,7 +16,7 @@ export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={{flex: 1}}>
-        <AddEntry/>
+        <History/>
       </View>
     </Provider>
   );
